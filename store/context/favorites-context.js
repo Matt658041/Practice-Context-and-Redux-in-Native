d2be4c7e5c07d5b622dbb72f24cb8262f7a4b310
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-11-17 09:45:46 -0600

    created context provider and wrappednavigation container

---
+++ store/context/favorites-context.js
@@ -0,0 +1,34 @@
+import { createContext, useState } from "react";
+
+export const FavoritesContext = createContext({
+  ids: [],
+  addFavorite: (id) => {},
+  removeFavorite: (id) => {},
+});
+
+export default function FavoritesContextProvider({ children }) {
+  const [favoriteMealIds, setFavoriteMealIds] = useState("");
+
+  function addFavorite(id) {
+    setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  }
+
+  function removeFavorite(is) {
+    setFavoriteMealIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId !== id)
+    );
+  }
+
+  const value = {
+    id: favoriteMealIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+
+  return (
+    <FavoritesContext.Provider value={value}>
+      {" "}
+      {children}
+    </FavoritesContext.Provider>
+  );
+}
