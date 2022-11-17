116aa0631a1dbc2fb6f988c328ec58158069e983
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-11-17 13:31:24 -0600

    used context instead of redux

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,38 @@
-import { Text } from 'react-native';
+import MealsList from "../components/MealsList/MealsList";
+import { useContext } from "react";
+import { FavoritesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
+import { View, Text , StyleSheet } from '@react-navigation/native';
 
 function FavoritesScreen() {
-  return <Text>The favorites screen!</Text>;
+  const favoriteMealsCtx = useContext(FavoritesContext);
+
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsCtx.ids.includes(meal.id)
+  );
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}> You have no favorites!</Text>
+      </View>
+    );
+  }
+
+  return <MealsList items={favoriteMeals} />;
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+});
